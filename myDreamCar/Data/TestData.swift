061f8b9c54ce6d0ab283ee9548199b5f5b513074
Dave <dcmoreland@hotmail.com>
2018-07-30 03:55:28 -0500

    working on data storage/retrieval

---
+++ myDreamCar/Data/TestData.swift
@@ -0,0 +1,38 @@
+//
+//  TestData.swift
+//  myDreamCar
+//
+//  Created by Dave on 7/30/18.
+//  Copyright © 2018 DigitalSuccess-Systems. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+/*  Test Data
+
+ Entity:
+ 1. Manufacturer
+ 2. Category
+ 3. Reviews
+ 4. Asset
+ 
+ 
+ Relationships
+ 
+ Manufacturer2Category
+ Manufacturer2Asset
+ Manufacturer2Review
+ 
+ Category2Manufacturer
+ Category2Review
+ 
+ Asset2Category
+ Asset2Manufacturer
+ Asset2Review
+ 
+ */
+
+
+
+
